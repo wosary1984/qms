866bdc365247cf7b934d296c61330c2d2db3719e
bruce wang <bruce.wang01@sap.com>
2019-06-27 15:01:18 +0800

    update login page

--- src/app/shared/login/login.component.ts
+++ src/app/shared/login/login.component.ts
@@ -2,7 +2,7 @@ import { Component, OnInit, Inject } from '@angular/core';
 import { Router } from '@angular/router';
 import { Auth } from 'src/app/model/entity';
 import { AuthService } from 'src/app/service/auth/auth.service';
-
+declare var $:any;
 @Component({
     selector: 'app-login',
     templateUrl: './login.component.html',
@@ -18,25 +18,36 @@ export class LoginComponent implements OnInit {
     }
 
     async ngOnInit() {
-        try {
-            const auth = await this.service.checkSession();
-            console.log(auth);
-            if (auth && !auth.hasError && auth.isLogged) {
-                this.router.navigate(['/']);
-            }
-        }
-        catch (error) {
-            let url: string = '/login';
-            sessionStorage.setItem('redirectUrl', url);
-            if (error.status == 0) {
-                this.router.navigate(['/error'], {
-                    queryParams: {
-                        errorMessage: error
-                    }
-                });
-                console.log(error);
-            }
-        }
+
+        // initial icheck style
+        $(function () {
+            $('input').iCheck({
+              checkboxClass: 'icheckbox_square-blue',
+              radioClass: 'iradio_square-blue',
+              increaseArea: '20%' /* optional */
+            });
+          });
+
+        return this.service.checkSession()
+          .then( auth => {
+              console.log( auth );
+              if ( auth && !auth.hasError && auth.isLogged ) {
+                  this.router.navigate( ['/'] );
+              }
+          } )
+          .catch(( error: any ) => {
+              let url: string = '/login';
+              sessionStorage.setItem( 'redirectUrl', url );
+
+              if ( error.status == 0 ) {
+                  this.router.navigate( ['/error'], {
+                      queryParams: {
+                          errorMessage: error
+                      }
+                  } );
+                  console.log( error );
+              }
+          } );
     }
 
     onSubmit(formValue: any): void {
@@ -51,7 +62,7 @@ export class LoginComponent implements OnInit {
                     //sessionStorage.setItem( 'username', String( auth.user.username ) );
                 } else {
 
-                    //this.auth = Object.assign( {}, auth );
+                    this.auth = Object.assign( {}, auth );
                 }
             });
 
